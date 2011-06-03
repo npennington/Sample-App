cc25b5ed9cb8d2ce229adc649d80f74195e4a6fe
Noel Pennington <npennington@gmail.com>
2011-06-03 11:13:01 -0500

    Added site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,47 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
   end
-
+  
+  it "should have the correct title" do
+    get 'home'
+    response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+  end
+  
+  it "should have non-blank data" do
+    get 'home'
+    response.body.should_not =~ /<body>\s*<\/body>/
+  end
+    
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
   end
+  
+   it "should have the correct title" do
+    get 'contact'
+    response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+  end
 
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+  
+   it "should have the correct title" do
+    get 'about'
+    response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+  end
 end
